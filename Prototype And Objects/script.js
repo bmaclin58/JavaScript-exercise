1e6ec726cac00e3e653cd289f7f33eae8806b04b
bmaclin58 <bmaclin58@gmail.com>
2019-03-21 22:53:00 -0400

    YES! Got it!  Cleaning up the code in the morning!
    GOOD NIGHT!

--- Prototype And Objects/script.js
+++ Prototype And Objects/script.js
@@ -18,73 +18,73 @@ var QUESTION = function (question, answer, options){
     this.choices = GenerateChoices(this.options);
 };
 function GenerateChoices(options){
+    //console.log("Generate Choices " + totalScore);
     var arrayStorage = [];
     for (index = 0; index < options.length; index++){
         arrayStorage.push((index + ": " + options[index]));
     }
     return arrayStorage;
 }
-
-function PrintQuestion(){
+//function GenerateQuestions (){
+    var question1 = new QUESTION ("Luke Skywaker is played by which actor?", "Mark Hamill" , ["Harrison Ford" , "Will Smith" , "Mark Hamill" , "Adam Driver"]);
+    var question2 = new QUESTION ("Python3 was released in what year?", 2008 , [2008 , 2000 , 1996 , 2010, 1990]);
+    var question3 = new QUESTION ("What was the first internet content search engine?", "Archie" , ["Google" , "Archie" , "AskJeeves" , "AOL", "Yahoo" , "KnowBot"]);
+    var totalScore= 0;
+    //store questions in an array....
+    var allQuestions = [question1,question2,question3];
+    console.log("Before PrintQuestion " + totalScore);
+    PrintQuestion(totalScore,allQuestions);
+    
+    console.log("Outside " + totalScore);
+//}
+function PrintQuestion(totalScore,allQuestions){
+    console.log("PrintQuestion "+ totalScore);
+//gives a random number based on the allQuestions array
+    var randomQuestion = Math.round(Math.random() * (allQuestions.length - 1));
+    //assigns the asked question to the random index in the array
+    var questionAsked = allQuestions[randomQuestion];
     console.log(questionAsked.question);
+    
     for (var index = 0; index < questionAsked.choices.length; index++){
         console.log(questionAsked.choices[index]);
     } 
     var selectedAnswer = prompt ("Select an answer (type the number): Type exit to quit");
-    CheckAnswer(selectedAnswer);
+    CheckAnswer(selectedAnswer,totalScore, questionAsked);
 }
 
-function GenerateNewQuestion(allQuestions){
-    console.log("GenerateNewQuestion" + totalScore);
-    return randomQuestion = Math.round(Math.random() * (allQuestions.length - 1));
-}
-
-function CheckAnswer(answer){
+function CheckAnswer(answer,totalScore,questionAsked){
     var desiredAnswer = questionAsked.answer;
 
-    console.log("Check Answer" + totalScore);
+    console.log("Check Answer " + totalScore);
     if (answer === "exit"){
         return;
     }
-
     else if (desiredAnswer == questionAsked.options[answer]){
         console.log("Answer is correct");
         totalScore++;
         CallScore(totalScore);
-        QUIZ();
+        PrintQuestion(totalScore,allQuestions);
 
     }
     else{
         console.log("Answer is incorrect");
         CallScore(totalScore);
-        QUIZ();
+        PrintQuestion(totalScore,allQuestions);
 
     }
 }
 
 function CallScore(totalScore){
-    console.log("Call Score" + totalScore);
+    console.log("Call Score " + totalScore);
     console.log("Your current score is: " + totalScore);
     console.log("--------------------");
 }
 
-var question1 = new QUESTION ("Luke Skywaker is played by which actor?", "Mark Hamill" , ["Harrison Ford" , "Will Smith" , "Mark Hamill" , "Adam Driver"]);
-var question2 = new QUESTION ("Python3 was released in what year?", 2008 , [2008 , 2000 , 1996 , 2010, 1990]);
-var question3 = new QUESTION ("What was the first internet content search engine?", "Archie" , ["Google" , "Archie" , "AskJeeves" , "AOL", "Yahoo" , "KnowBot"]);
-
-//store questions in an array....
-var allQuestions = [question1,question2,question3];
-console.log("Outside 1" + totalScore);
-GenerateNewQuestion(allQuestions);
-console.log("Outside 2" + totalScore);
-var questionAsked = allQuestions[randomQuestion];
-console.log("Outside 3" + totalScore);
-var randomQuestion;
-PrintQuestion();
-console.log("Outside 4" + totalScore);
-
-var totalScore = 0;
+PrintQuestion(totalScore,allQuestions);
+console.log("WeOutside");
 
 //will need to create a function that runs a check on the value of the answer and the value of the chosen array.  The prompt will save the index spot of the answers
 }
 QUIZ();
+
+// while answer != exit keep doing
